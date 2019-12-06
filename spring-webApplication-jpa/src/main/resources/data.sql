4e412f6ad32f826d67f615994350421a4fbeb1ac
Prathvi Shetty <prathvi.shetty@Triconinfotech.com>
2019-12-06 16:39:09 +0530

    Spring boot code with jpa

---
+++ spring-webApplication-jpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into phone values(101, 'Motorola One power','airtel');
+insert into phone values(102, 'Samsung','vodaphone');
+insert into phone values(103, 'Iphone','airtel');
+insert into phone values(104, 'Pixel','airtel');
+insert into phone values(105, 'Redmi','airtel');
---
+++ spring-webApplication-jpa/src/test/java/com/example/springwebApplicationjpa/SpringWebApplicationJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebApplicationjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
